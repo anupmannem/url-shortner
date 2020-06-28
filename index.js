dae37256e307710817ad13bb31da533261b9bad0
anupmannem <anupmannem@gmail.com>
2020-06-28 11:09:25 +0530

    post route setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const yup = require('yup')
+const { nanoid } = require('nanoid')
 
 const app = express()
 
@@ -19,9 +21,42 @@ app.use(express.static('./public'))
 //     // TODO: redirect to url
 // })
 
-// app.post('/url', (req, res) => {
-//     // TODO: create a short url
-// })
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+})
+
+app.post('/url', async (req, res, next) => {
+    let { slug, url } = req.body
+    try {
+        await schema.validate({
+            slug,
+            url
+        })
+        if (!slug) {
+            slug = nanoid(5)
+        }
+        slug = slug.toLowerCase()
+        res.json({
+            slug,
+            url
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status)
+    } else {
+        res.status(500)
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'OK!!' : error.stack
+    })
+})
 
 const port = process.env.PORT || 1337
 app.listen(port, () => console.log(`listening at http://localhost:${port}`))
