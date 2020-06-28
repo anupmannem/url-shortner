918546e62d652072746c3c5d5678d81660b8292a
anupmannem <anupmannem@gmail.com>
2020-06-28 10:17:59 +0530

    test ready for heroku

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+
+const app = express()
+
+app.use(helmet())
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.static('./public'))
+
+// app.get('/url/:id', (req, res) => {
+//     // TODO: get the short url
+// })
+
+// app.get('/:id', (req, res) => {
+//     // TODO: redirect to url
+// })
+
+// app.post('/url', (req, res) => {
+//     // TODO: create a short url
+// })
+
+const port = process.env.PORT || 1337
+app.listen(port, () => console.log(`listening at http://localhost:${port}`))
